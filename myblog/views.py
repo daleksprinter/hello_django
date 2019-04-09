2436abfbced6b855aab7ce1fbfe6fd7a19870231
daleksprinter <b1015120@fun.ac.jp>
2019-04-09 17:00:21 +0900

    use template view

--- myblog/views.py
+++ myblog/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from myblog.models import Item
 from django.shortcuts import render
-def index(request):
-    context = {
-        'items' : Item.objects.all(),
-    }
-    return render(request, 'myblog/index.html', context)
+from django.views.generic import TemplateView
+
+class Index(TemplateView):
+    template_name = 'myblog/index.html'
+    def get_context_data(self, **kwargs):
+        contest = {
+            'items' : Item.objects.all(),
+        }
+        return contest
